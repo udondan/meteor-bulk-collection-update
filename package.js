952b92bb88920b924c0001c6f606fa091e93c46f
udondan <deemes79@gmail.com>
2014-10-07 15:38:36 +0200

    Migrated to meteor >0.9.0

--- package.js
+++ package.js
@@ -1,9 +1,12 @@
 Package.describe({
   summary: "Bulk insert/update/delete documents in a collection",
+  version: "0.2.0",
+  git: "https://github.com/udondan/meteor-bulk-collection-update.git",
   environments: ['server']
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   where = 'server';
   api.add_files('lib/bulkCollectionUpdate.js', where);
   api.export && api.export('bulkCollectionUpdate', where);
